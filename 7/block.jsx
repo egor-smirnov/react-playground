5cc96e03a6f1f2deec350f210b489e2befc8babd
Egor Smirnov <egorsmir@gmail.com>
2015-05-01 16:47:51 +0200

    More on React mount / unmount

--- 7/block.jsx
+++ 7/block.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+var interval;
+
 class Block extends React.Component {
 
     constructor() {
@@ -18,19 +20,24 @@ class Block extends React.Component {
     render() {
         console.log('rendering');
         return (
-            <button onClick={this.update.bind(this)}>{this.state.value}</button>
+            <button onClick={this.update.bind(this)}>{this.state.value * this.state.multiplier}</button>
         );
     }
 
     componentWillMount() {
+        this.setState({
+            multiplier: 2
+        });
         console.log('mounting');
     }
 
     componentDidMount() {
+        interval = setInterval(this.update.bind(this), 500);
         console.log('mounted');
     }
 
     componentWillUnmount() {
+        clearInterval(interval);
         console.log('unmounted');
     }
 }
